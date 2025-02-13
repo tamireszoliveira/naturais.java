6a232c2cb286c4f51b1289c112e3803b5b2f2095
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-13 15:25:12 -0300

    mudança enunciado

--- src/controller/NaturaisController.java
+++ src/controller/NaturaisController.java
@@ -1,10 +1,7 @@
 package controller;
 
 public class NaturaisController {
-	/* 1. Criar uma aplicação em Java que tenha uma função recursiva que calcule o somatório do N primeiros
-número NATURAIS (a função deve retornar zero para números nega�vos)
-O Código deve apresentar, em formato de comentário, como foi definida a condição de parada;
-O Código deve apresentar, em formato de comentário, como foi definida a relação de chamada dos passos; */
+	
 	
 	public 	NaturaisController() {
 		super();
@@ -20,7 +17,4 @@ O Código deve apresentar, em formato de comentário, como foi definida a relaç
 		}
 		
 	}
-	
-
-
 }
